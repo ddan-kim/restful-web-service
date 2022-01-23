2633b88d84001619d87a33b1fd1341619aa97dd6
hj.kim <1q2w3e4r5t^>
2022-01-23 22:34:53 +0900

    Swagger 를 통한 API Documentation

--- src/main/java/com/study/restfulwebservice/user/User.java
+++ src/main/java/com/study/restfulwebservice/user/User.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,18 +21,23 @@ import static com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter.f
 @NoArgsConstructor
 //@JsonIgnoreProperties(value = {"password", "ssn"}) // 해당 속성을 class 단위에서 제어
 //@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
 
     private Integer id;
 
     /* javax validation */
     @Size(min = 2, message = "Name은 2글자 이상 입력해주세요.")
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
     @Past
+    @ApiModelProperty(notes = "사용자 등록일 입력해 주세요.")
     private Date joinDate;
 
     // @JsonIgnore 필드값 제어
+    @ApiModelProperty(notes = "사용자 비밀번호를 입력해 주세요.")
     private String password;
+    @ApiModelProperty(notes = "사용자 주민번호를 입력해 주세요.")
     private String ssn;
 
     public static SimpleBeanPropertyFilter userInfo() {
